ae5b83f13c10a27d9d0dde65a0f7ba3a43c77963
jordanwilcken@github.com <jordanwilcken@gmail.com>
2015-07-01 20:10:21 +0000

    Solved my last event before recovery thing

--- count.recovered.at.location.of.last.precious.event.sql
+++ count.recovered.at.location.of.last.precious.event.sql
@@ -19,3 +19,23 @@ WHERE location.location_code =
      )
    WHERE ROWNUM = '1'
   );
+
+SELECT precious_description,
+             last_event.event_timestamp,
+             last_event.event_location,
+             recovery_timestamp,
+             location_description AS recov_location,
+             ROW_NUMBER() OVER (PARTITION BY precious_id, recovery_timestamp ORDER BY event_timestamp DESC) as row_num
+FROM precious
+  INNER JOIN (
+                        SELECT precious_event.precious_id,
+                                     event_timestamp,
+                                     location_description AS event_location
+                        FROM event
+                          INNER JOIN precious_event ON event.event_id = precious_event.event_id
+                          INNER JOIN recovery ON precious_event.precious_id = recovery.precious_id
+                          INNER JOIN location ON recovery.location_code = location.location_code
+                       WHERE event.event_timestamp < recovery.recovery_timestamp
+                      ) last_event ON precious.precious_id = last_event.precious_id
+  INNER JOIN recovery ON precious.precious_id = recovery.precious_id
+  INNER JOIN location ON recovery.location_code = location.location_code;
